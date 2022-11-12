067908c4dea2bb7ef1cad3914eb99b617434bbc6
finwe-snowflakelearn <116255529+finwe-snowflakelearn@users.noreply.github.com>
2022-11-12 14:26:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ streamlit.dataframe(fruits_to_show)
 
 # Lesson 9
 # new section to display fruitvice api responce
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
